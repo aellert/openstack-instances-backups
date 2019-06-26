c1e49437b5cbc2abd017b53f025da83871bf8ddc
Alexandre Ellert <aellert@numeezy.com>
2019-06-26 08:40:56 +0200

    :bug: Fix keep 7 snapshots max

--- count_volume_snapshots.sh
+++ count_volume_snapshots.sh
@@ -2,7 +2,7 @@
 ROTATION="${1}"
 declare -A ARRAY
 
-if output=$(nova volume-list | awk -F'|' '/\|/ && !/ID/{system("echo "$2"")}'); then
+if output=$(openstack volume list | awk -F'|' '/\|/ && !/ID/{system("echo "$2"")}'); then
   set -- "$output"
   IFS=$'\n'; declare arrOutput=($*)
   for volume in "${arrOutput[@]}"; do
@@ -13,7 +13,7 @@ else
   echo "NO INSTANCE FOUND"
 fi
 
-if output=$(nova volume-snapshot-list | awk -F'|' '/\|/ && !/ID/{system("echo "$5"__"$2"__"$3"")}' | sort -n); then
+if output=$(openstack snapshot list | awk -F'|' '/\|/ && !/ID/{system("echo "$4"__"$2"__"$3"")}' | sort -n); then
   set -- "$output"
   IFS=$'\n'; declare arrOutput=($*)
 
@@ -22,22 +22,24 @@ if output=$(nova volume-snapshot-list | awk -F'|' '/\|/ && !/ID/{system("echo "$
   	IFS=__; declare arrVolume=($*)
   	# snapshot UUID
     SNAPSHOT_UUID="${arrVolume[2]:1:${#arrVolume[2]}-1}"
+    #echo "SNAPSHOT_UUID : $SNAPSHOT_UUID"
 
     # volume UUID
     VOLUME_UUID="${arrVolume[4]:1:${#arrVolume[4]}-1}"
+    #echo "VOLUME_UUID: $VOLUME_UUID"
 
     ARRAY[count__${VOLUME_UUID:0:8}]+="${SNAPSHOT_UUID}"
+    #echo "ARRAY: ${ARRAY[@]}"
   done
 
   # Get the counts and if some volumes get more than `rotation` backups we've to remove the older one
   for K in "${!ARRAY[@]}"; do
-  	STRINGLENGTH=${#ARRAY[$K][@]}
+  	STRINGLENGTH=${#ARRAY[$K]}
   	LENGTH="$((STRINGLENGTH/36))"
-
-  	echo "length $K:::${LENGTH}---${ARRAY[$K]}"
   	if [ "${LENGTH}" -gt "${ROTATION}" ]; then
   		echo "$K has to remove its older backup :: ${ARRAY[$K]:0:36}"
-  		nova volume-snapshot-delete "${ARRAY[$K]:0:36}"
+  		openstack snapshot delete "${ARRAY[$K]:0:36}"
+  		echo "openstack snapshot delete ${ARRAY[$K]:0:36}"
   	fi
   done
 else
